69c5b854438329223ff771f4001042db981ebdd9
LateNightKid <paveltessman@gmail.com>
2022-05-28 16:33:55 +0300

    admin chat id moved into env var

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ OUTLINE_DOWNLOAD_LINK = 'https://getoutline.org/ru/get-started/'
 
 #Monitoring bot settings
 MONITOR_API_TOKEN = os.getenv("MONITOR_API_TOKEN")
-ADMIN_CHAT_ID = '799341092'
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 STATUS_CHECK_FREQUENCY = 60*30 #this value should be in seconds. Now it's one message per 30 mins
 
